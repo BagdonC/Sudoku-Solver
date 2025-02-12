f676af3143c80875064f7c4ef40b04f54e406f1d
Chris <chriswasgehtsdichan@gmail.com>
2025-02-12 02:41:19 +0100

    Projektdateien hinzufügen.

---
+++ Sudoku Solver/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Sudoku_Solver.Logic;
+
+namespace Sudoku_Solver
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Solver Solver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Solver = new Solver();
+        }
+    }
+}
